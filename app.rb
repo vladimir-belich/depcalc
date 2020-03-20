c860a236c4a210493c1b36b5e44df0d517f2de3a
Vladimir Belich <vladimir.belich@gmail.com>
2020-03-20 17:11:09 +0200

    add views/tabl.erb

--- app.rb
+++ app.rb
@@ -12,5 +12,11 @@ class App < Sinatra::Base
     sum = params[:sum].to_f
     interest_rate = params[:interest_rate].to_f
     capitalization_method = params[:capitalization_method].to_i
+    
+    erb :tabl, locals: { months: months,
+                         date: start_date,
+                         sum: sum,
+                         interest_rate: interest_rate,
+                         capitalization_method: capitalization_method }
   end  
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
-describe 'Sinatra Application' do
-  it 'should allow accessing the home page' do
-    get '/'
-    expect(last_response).to be_ok
+describe App do
+  describe 'GET /' do
+    it 'should return the calculator page' do
+      get '/'
+      expect(last_response).to be_ok
+    end
+  end
+
+  describe 'POST #tabl' do
+    it 'should return the page with the calculation' do
+      post '/tabl', { start_date: '16-03-2020', months: 3, interest_rate: 13, start_sum: 30000.00, capitalization_method: 0 }
+      expect(last_response).to be_ok
+    end
   end
 end
