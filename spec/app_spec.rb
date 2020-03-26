9f12fb6eb68cc28ea4fd1120f64b6e0da23687c4
Vladimir Belich <vladimir.belich@gmail.com>
2020-03-26 17:13:50 +0200

    update app_spec.rb

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,4 +7,11 @@ describe App do
       expect(last_response).to be_ok
     end
   end
+
+  describe 'GET #table' do
+    it 'should return the page with the calculation' do
+      get '/table', { start_date: '16-03-2020', months: 3, interest_rate: 13, start_sum: 30000.00, capitalization_method: 0 }
+      expect(last_response).to be_ok
+    end
+  end
 end
