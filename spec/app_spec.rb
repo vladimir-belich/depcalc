0cf3b44bb1c38fc80c01bf4ee083c7b4eafe1d9d
Vladimir Belich <vladimir.belich@gmail.com>
2020-03-28 01:56:22 +0200

    add test 'Download CSV'

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -14,4 +14,16 @@ describe App do
       expect(last_response).to be_ok
     end
   end
+
+  describe 'Download CSV' do
+    it 'should return a CSV file with the calculation' do
+      get '/file', { start_date: '16-03-2020', months: 3, interest_rate: 13, start_sum: 30000.00, capitalization_method: 0 }
+      
+      filename = last_request.env['rack.session']['session_id'].to_s[0..8] + '.csv'
+
+      expect(last_response).to be_ok
+      expect(last_response.content_type).to eq 'Application/octet-stream'
+      expect(last_response.headers['Content-Disposition']).to include(filename)
+    end
+  end 
 end
